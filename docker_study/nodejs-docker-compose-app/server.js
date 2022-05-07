3ca130002f99b56a191f63d087b69d620628f231
JongwonAhn <azsx1001202@naver.com>
2022-05-07 23:58:44 +0900

    'dockerStudy6'

--- docker_study/nodejs-docker-compose-app/server.js
+++ docker_study/nodejs-docker-compose-app/server.js
@@ -1,24 +1,25 @@
 const express = require("express");
 const redis = require("redis");
-//레디스 클라이언트 생성
+//레디스 클라이언트 생성 
 const client = redis.createClient({
     host: "redis-server",
     port: 6379
-});
+})
 
-const app = express(); 
+const app = express();
 
-//숫자는 0부터 시작
+//숫자는 0 부터 시작합니다.
 client.set("number", 0);
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     client.get("number", (err, number) => {
-        //현재 숫자를 가져온 후에 1씩 올려준다
+        //현재 숫자를 가져온 후에 1씩 올려줍니다.
+        res.status(number >= 100 && number < 600 ? err.code : 500);
+        //dres.send("숫자가 1씩 올라갑니다. 숫자: " + number)
         client.set("number", parseInt(number) + 1)
-        //res.send("숫자가 1씩 올라 갑니다" + toString(parseInt(number)))
-    });
-});
-  
+    })
+})
+
 
 app.listen(8080);
-console.log("server is running");
+console.log('Server is running');
